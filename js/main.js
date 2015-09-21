ba3d8241e8e2139c21599d2746845aefff45833b
wandrinstar <m.edson373@gmail.com>
2015-09-21 13:03:39 +0700

    Edit

--- js/main.js
+++ js/main.js
@@ -1,14 +1,8 @@
 function Cat (imgURL, name, id) {
   this.imgURL = imgURL;
-  this.numClicks = 0;
-  this.catID = id;
   this.catName = name;
-  this.nameDiv = '<div class="name-div">' + this.catName + '</div>';
-  this.displayDiv = '<div class="display-div"' + ' id="cat-' + this.catID + '">' +
-                '<div class="cat-name">' + this.catName + '</div>' +
-                '<div class="clicks-counter">' + this.numClicks + '</div>' +
-                '<img class="cat-image" src="' + this.imgURL + '">' +
-                '</>';
+  this.catID = id;
+  this.numClicks = 0;
 };
 
 var catData = [["https://lh3.ggpht.com/kixazxoJ2ufl3ACj2I85Xsy-Rfog97BM75ZiLaX02KgeYramAEqlEHqPC3rKqdQj4C1VFnXXryadFs1J9A=s0#w=640&h=496", 'Hal'],
@@ -27,23 +21,7 @@ function createAllCats() {
 
 $(document).ready(function(){
   createAllCats();
-  for (var i in allCats) {
-    $('#cat-list').append(allCats[i].nameDiv).on('click', { i: i}, (function(event) {
-      var iCopy = event.data.i;
-      return function(iCopy) {
-        alert(iCopy);
-      };
-    }
-  ))
-  };
 });
 
-//event listener for number of clicks
-$(document).ready(function() {
-  $('.cat-image').click(function() {
-    var catID = $(event.target).parent().attr('id');
-    var cat = allCats[catID.charAt(catID.length-1)];
-    cat.numClicks += 1;
-    $(event.target).siblings('.clicks-counter').text(cat.numClicks);
-  })
-})
+
+
